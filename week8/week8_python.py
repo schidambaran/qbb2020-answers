ed15c8600db985641b57ab4881486a20492107bf
Sadhana Chidambaran <39743353+schidambaran@users.noreply.github.com>
2020-11-29 11:41:48 -0500

    Add UMAPs (complete)

--- week8/week8_python.py
+++ week8/week8_python.py
@@ -41,3 +41,58 @@ sc.pl.rank_genes_groups(adata, groupby = "leiden", save = "_t_test.png")
 # Identify genes that distinguish each cluster using the logistic regression method
 sc.tl.rank_genes_groups(adata, groupby = "leiden", method = "logreg")
 sc.pl.rank_genes_groups(adata, groupby = "leiden", save = "_logreg.png")
+
+# Create UMAPs showing which clusters are enriched for certain genes and add info to dataset
+
+sc.tl.leiden(adata, key_added = "clusters")
+
+# Microglia (cluster 22)
+sc.tl.umap(adata)
+sc.pl.umap(adata, color = "Ccl4", save = "_Ccl4.png")
+adata.obs["clusters"] = adata.obs["clusters"].replace("22", "Microglia")
+
+# Oligodendrocytes (cluster 17)
+sc.tl.umap(adata)
+sc.pl.umap(adata, color = "Olig2", save = "_Olig2.png")
+adata.obs["clusters"] = adata.obs["clusters"].replace("17", "Oligodendrocytes")
+
+# Erythrocytes (cluster 14)
+sc.tl.umap(adata)
+sc.pl.umap(adata, color = "Hbb-bs", save = "_Hbb-bs.png")
+adata.obs["clusters"] = adata.obs["clusters"].replace("14", "Erythrocytes")
+
+# Purkinje neurons (cluster 20)
+sc.tl.umap(adata)
+sc.pl.umap(adata, color = "Reln", save = "_Reln.png")
+
+sc.tl.umap(adata)
+sc.pl.umap(adata, color = "Lhx1", save = "_Lhx1.png")
+
+adata.obs["clusters"] = adata.obs["clusters"].replace("20", "Purkinje Neurons")
+
+# Endothelial cells (cluster 18)
+sc.tl.umap(adata)
+sc.pl.umap(adata, color = "Cldn5", save = "_Cldn5.png")
+adata.obs["clusters"] = adata.obs["clusters"].replace("18", "Endothelial Cells")
+
+# Astrocytes (clusters 6 and 15)
+sc.tl.umap(adata)
+sc.pl.umap(adata, color = "Aldoc", save = "_Aldoc.png")
+
+sc.tl.umap(adata)
+sc.pl.umap(adata, color = "Ndrg2", save = "_Ndrg2.png")
+
+adata.obs["clusters"] = adata.obs["clusters"].replace("6", "Astrocytes")
+adata.obs["clusters"] = adata.obs["clusters"].replace("15", "Astrocytes")
+
+# Intermediate progenitor cells (cluster 2)
+sc.tl.umap(adata)
+sc.pl.umap(adata, color = "Eomes", save = "_Eomes.png")
+adata.obs["clusters"] = adata.obs["clusters"].replace("2", "Intermediate Progenitor Cells")
+
+# Neural progenitor cells (cluster 10)
+sc.tl.umap(adata)
+sc.pl.umap(adata, color = "H2afz", save = "_H2afz.png")
+adata.obs["clusters"] = adata.obs["clusters"].replace("10", "Neural Progenitor Cells")
+
+sc.pl.umap(adata, color = "clusters", save = "_final.png")
